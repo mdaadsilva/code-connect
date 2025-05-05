f043bf219c970cc778095703bda1fd1c352df91c
Martin <martindealmeida17@gmail.com>
2025-05-05 19:10:58 -0300

    feat: arrumando estilo do card

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
+import styles from "./avatar.module.css";
+
 
 export const Avatar = ({ name, imageSrc }) => {
-  return (
-    <ul>
-      <li>
-        <Image
-          src={imageSrc}
-          width={32}
-          height={32}
-          alt={`Avatar do(a) ${name}`}
-        />
-      </li>
-      <li>@{name}</li>
+return (
+    <ul className={styles.avatar}>
+        <li>
+            <Image
+                src={imageSrc}
+                width={32}
+                height={32}
+                alt={`Avatar do(a) ${name}`}
+            />
+        </li>
+        <li>@{name}</li>
     </ul>
-  );
+);
 };
