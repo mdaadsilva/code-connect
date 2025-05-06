f30f923368b7bb63984afd08f47ce00cb4f29b03
Martin <martindealmeida17@gmail.com>
2025-05-06 14:56:20 -0300

    feat: passando paginas como parametros

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import { CardPost } from "@/components/CardPost";
 import logger from "@/logger";
 import Styles from "./page.module.css";
+import Link from "next/link";
 
 async function getAllPost(page) {
  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
@@ -13,10 +14,12 @@ async function getAllPost(page) {
 }
 
 export default async function Home() {
-  const {data: posts} = await getAllPost(1)
+  const {data: posts, prev, next } = await getAllPost(1)
   return (
     <main className={Styles.postsContainer}>
       {posts.map(post => <CardPost key={post.id} post={post} />)}
+      {prev && <Link href={`/?page=${prev}`}>Página Anterior</Link>}
+      {next && <Link href={`/?page=${next}`}>Proxima página</Link>}
     </main>
   );
 }
