4621f27522d67488c4a0f06bc0c212b928cb749a
Martin <martindealmeida17@gmail.com>
2025-05-06 15:36:29 -0300

    feat: moctando os grids dos cards com uma integração de uma api

--- src/app/page.js
+++ src/app/page.js
@@ -4,22 +4,37 @@ import Styles from "./page.module.css";
 import Link from "next/link";
 
 async function getAllPost(page) {
- const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
- if(!response.ok){
-  logger.error('Ops. alguma coisa ocorreu mal')
-  return []
- }
- logger.info('Posts carregados com sucesso')
-  return response.json() 
+  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`, {
+    cache: "no-store"
+  });
+
+  if (!response.ok) {
+    logger.error("Erro ao buscar posts.");
+    return { data: [], prev: null, next: null };
+  }
+
+  const json = await response.json();
+  const currentPage = parseInt(page, 10);
+
+  return {
+    data: json.data,
+    prev: currentPage > 1 ? currentPage - 1 : null,
+    next: json.data.length === 6 ? currentPage + 1 : null
+  };
 }
 
-export default async function Home() {
-  const {data: posts, prev, next } = await getAllPost(1)
+export default async function Home({ searchParams }) {
+  const CurrentPage = parseInt(searchParams?.page || "1", 10);
+  const { data: posts, prev, next } = await getAllPost(CurrentPage);
+
   return (
     <main className={Styles.postsContainer}>
       {posts.map(post => <CardPost key={post.id} post={post} />)}
-      {prev && <Link href={`/?page=${prev}`}>Página Anterior</Link>}
-      {next && <Link href={`/?page=${next}`}>Proxima página</Link>}
+
+      <div className={Styles.links}>
+        {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
+        {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+      </div>
     </main>
   );
 }
